e35e2451aa5f761a2befa22e3e81a4a1907da9ad
Hasindu Ranasinghe <152050830+hasindur@users.noreply.github.com>
2025-08-16 13:07:19 +0530

    login section

--- controller/userController.js
+++ controller/userController.js
@@ -16,7 +16,7 @@ export function saveUser(req, res) {
         .then((data) => {
             res.status(201).json({
                 message: "User created successfully",
-                user: data
+               
             });
         })
         .catch((error) => {
@@ -28,4 +28,31 @@ export function saveUser(req, res) {
         });
    
    
+}
+
+export function loginUser(req, res) {
+    const email = req.body.email;
+    const password = req.body.password;
+
+    User.findOne({ email: email })
+        .then((user) => {
+           
+            if (!user) {
+                return res.status(404).json({ message: "User not found" });
+            }
+
+            const isPasswordValid = bcrypt.compareSync(password, user.password);
+            if (!isPasswordValid) {
+                return res.status(401).json({ message: "Invalid password" });
+            }
+
+            res.status(200).json({
+                message: "Login successful",
+                
+            });
+        })
+        .catch((error) => {
+            console.error("Error during login:", error);
+            res.status(500).json({ message: "Internal server error" });
+        });
 }
