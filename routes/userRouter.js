3b041d7baa6b241177b6bb1e2f571d9a1d5c41c0
Hasindu Ranasinghe <152050830+hasindur@users.noreply.github.com>
2025-08-16 12:43:24 +0530

    add dependencies

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -1,2 +1,15 @@
 import express from "express";
-import userController from "../controllers/userController.js";
+import { saveUser } from "../controller/userController.js";
+
+
+
+
+const userRouter = express.Router();
+
+userRouter.post("/", saveUser);
+// userRouter.get("/users", userController.getAllUsers);
+// userRouter.get("/users/:id", userController.getUserById);
+// userRouter.put("/users/:id", userController.updateUser);      //edit
+// userRouter.delete("/users/:id", userController.deleteUser);
+
+export default userRouter;
