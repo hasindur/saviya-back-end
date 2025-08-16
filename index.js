8e5f53e16f8785ef7c9e8b806b9efdeae7fd020a
Hasindu Ranasinghe <152050830+hasindur@users.noreply.github.com>
2025-08-16 12:06:01 +0530

    get dependencies

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 import express from "express";
 import mongoose from "mongoose";
+import bodyParser from "body-parser";
 import cors from "cors";
 import dotenv from "dotenv";
+import userRouter from "./routes/userRouter.js";
 
 dotenv.config();
 const app = express();
 app.use(cors());
 app.use(express.json());
+
 
 // ✅ MongoDB connection
 mongoose.connect(process.env.MONGO_URI, {
---
+++ routes/userRouter.js
@@ -0,0 +1,2 @@
+import express from "express";
+import userController from "../controllers/userController.js";
